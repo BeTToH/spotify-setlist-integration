1301700d6ba4a1746f6666527488e905dfd38cc7
BeTToH <bebeto.fb@hotmail.com>
2022-05-22 21:43:41 -0300

    ref: adds docstrings and comments and applies linter

--- main.py
+++ main.py
@@ -5,24 +5,35 @@ from bs4 import BeautifulSoup
 from spotipy import Spotify
 import spotipy.util as util
 
-def get_html(url):
-    res = requests.get(url)    
+
+def get_html(url: str):
+    """
+        Get the html of a page
+
+        Args:
+            url (str): url of the page
+
+        Returns:
+            str: the html of the page
+    """
+    res = requests.get(url)
     html = res.text
     return html
+
 
 def search_for_setlist(artist: str, pos: int = 1) -> Tuple[str, str, str]:
     """
         Searches for an artist setlist on setlist.fm
 
         Args:
-            - artist (str): artist name to be searched
+            - artist (str): artist name to search
             - pos (int): position on list returned from search
 
         Returns:
             Tuple[str, str, str]:
                 first element is the concert name,
                 second is the link to the setlist, and
-                third is the artist name on the setlist web page            
+                third is the artist name on the setlist web page
     """
     artist = artist.replace(" ", "+")
 
@@ -38,92 +49,126 @@ def search_for_setlist(artist: str, pos: int = 1) -> Tuple[str, str, str]:
 
     return last_concert.text, last_concert['href'], artist.text
 
-def get_setlist(artist: str):
-    setlist = []
+
+def get_setlist_by_artist(artist: str) -> Tuple[str, list]:
+    """
+        Gets the last setlist with at least 7 songs of an artist.
+
+        Args:
+            - artist (str): artist name to search
+
+        Returns:
+            tuple[str, list[str]]:
+                tuple which the first element is the concert's title and
+                the second element is a list of songs - setlist.
+    """
     pos = 1
+    setlist = []
     while len(setlist) < 6:
-        setlist = []
-        last_concert_title, last_concert_link, artist_name = search_for_setlist(artist, pos)
+        concert_title, concert_link, artist_name = search_for_setlist(artist, pos)
         if artist_name.lower() != artist.lower():
             pos += 1
             continue
-        last_concert_link = "https://www.setlist.fm/" + last_concert_link
-        setlist_page_html = get_html(last_concert_link)
+        concert_link = "https://www.setlist.fm/" + concert_link
+        setlist = get_setlist(concert_link)
+        pos += 1
+
+    return concert_title, setlist
+
+
+def get_setlist(concert_url: str):
+    """
+        Gets a setlist by the url
+
+        Args:
+            - concert_url (str): url of the concert to get the setlist from
+
+        Returns:
+            list: the setlist - a list of songs
+    """
+    # TODO - add better exception treatment
+    try:
+        setlist = []
+        setlist_page_html = get_html(concert_url)
         setlist_page = BeautifulSoup(setlist_page_html)
         setlist_div = setlist_page.find_all('div', {'class': 'setlistList'})[0]
         setlist_html = setlist_div.find_all('li', {'class': 'setlistParts song'})
         for li in setlist_html:
             song = li.find_all('a')[0].text
             setlist.append(song)
-        pos += 1
+    except Exception:
+        pass
+    # TODO - get concert name
 
-    return last_concert_title, setlist
+    return setlist
 
 
 def rmv_special_chars(string: str):
     return ''.join(e for e in string if e.isalnum())
 
 
-if __name__ == "__main__":    
-    # get setlist songs
-    artists = ['drake'] 
-   
+if __name__ == "__main__":
+    ARTISTS = ['drake']
+    SEARCH_LIMIT = 5
+
+    # env vars
     config = dotenv_values()
     CLIENT_ID = config["CLIENT_ID"]
-    CLIENT_SECRET= config["CLIENT_SECRET"]
+    CLIENT_SECRET = config["CLIENT_SECRET"]
     REDIRECT_URI = config["REDIRECT_URI"]
-    scope = "user-library-read"
-    token = util.prompt_for_user_token(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, 
-    redirect_uri=REDIRECT_URI, scope="user-library-read playlist-modify-private playlist-modify-public")
+
+    SCOPE = "user-library-read playlist-modify-private playlist-modify-public"
+    token = util.prompt_for_user_token(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
+                                       redirect_uri=REDIRECT_URI, scope=SCOPE)
     sp = Spotify(token)
-    CURRENT_USER_ID = sp.current_user()['id']  
-    LIMIT = 5
+    CURRENT_USER_ID = sp.current_user()['id']
 
     playlist_name = ""
     songs = []
     songs_found = []
-    songs_not_found = []
-    
-    for artist in artists:
-        title, setlist = get_setlist(artist)
+
+    for artist in ARTISTS:
+        title, setlist = get_setlist_by_artist(artist)
         songs.extend(setlist)
-        playlist_name += title + " \ "
+        playlist_name += title + " \\ "
 
         # add setlist songs to that playlist
         song_ids = []
-        
+
         for song in songs:
             print(f"Trying to find song: {song} from {artist}")
-            results = sp.search(q=f"{song} {artist}", limit=LIMIT)
-                        
-            returned_songs = results['tracks']['items']
+            first_song = sp.search(q=f"{song} {artist}", limit=SEARCH_LIMIT)
+            returned_songs = first_song['tracks']['items']
 
+            # Try to find song with the exact same name
             was_found = False
             for current_song in returned_songs:
                 current_song_name = rmv_special_chars(current_song['name']).lower()
                 formatted_song = rmv_special_chars(song).lower()
                 if current_song_name == formatted_song:
-                    if any(c['name'].lower() == artist.lower() for c in current_song['artists']):
+                    if any(c['name'].lower() == artist.lower()
+                           for c in current_song['artists']):
                         song_ids.append(current_song['id'])
                         print(f"{song} was found")
                         songs_found.append(song)
                         was_found = True
                         break
 
+            # If song with the exact same name wasn't found,
+            # gets the first one on spotify search
             if not was_found:
-                results = sp.search(q=f"{song} {artist}", limit=1)['tracks']['items']
-                first_song_artists = [artist['name'] for artist in results[0]['artists']]
-                first_song_name = f"{results[0]['name']} from {', '.join(first_song_artists)}"
-                print(f"{song} wasn't found. "
-                      f"Instead, added {first_song_name}")
+                first_song = sp.search(q=f"{song} {artist}", limit=1)['tracks']['items']
+                first_song_artists = [artist['name'] for artist in first_song[0]['artists']]
+                first_song_name = (f"{first_song[0]['name']} from "
+                                   f"{', '.join(first_song_artists)}")
                 songs_found.append(first_song_name)
+                print(f"{song} from {artist} wasn't found. "
+                      f"Instead, added {first_song_name}")
 
-    playlist_name = playlist_name[:-3]
-      
+    playlist_name = playlist_name[:-3]  # Remove the \ on the final of the string
+
     playlist = sp.user_playlist_create(CURRENT_USER_ID, playlist_name)
 
     sp.playlist_add_items(playlist['id'], song_ids)
     print(f"Created playlist {playlist_name} with songs:")
     print('\n'.join([f"\t--{song}" for song in songs_found]))
-
-    print(f"Songs not found: {', '.join(songs_not_found)}")
